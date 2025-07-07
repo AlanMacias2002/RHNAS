d0b34490fda0a926401f1514b05419fcdc8d92b5
AlanMacias2002 <89363346+AlanMacias2002@users.noreply.github.com>
2025-07-07 12:59:34 -0600

    Update rhimm.sh

--- rhimm.sh
+++ rhimm.sh
@@ -9,11 +9,8 @@ lvcreate -l 100%FREE --name repoveeam repoimm
 mkfs.xfs -b size=4096 -m reflink=1,crc=1 /dev/repoimm/repoveeam
 mkdir /repoveeam
 mount /dev/repoimm/repoveeam /repoveeam
-adduser veeamrepo
-echo "****** Please Enter veeamrepo Password ******"
-passwd veeamrepo
 mkdir /repoveeam/backups
-chown veeamrepo:veeamrepo /repoveeam/backups
+chown adminbkp:adminbkp /repoveeam/backups
 chmod 700 /repoveeam/backups
 UUID=$(blkid | grep repoimm-repoveeam |cut -f2 -d'='|cut -f2 -d'"')
 echo "******Saving /etc/fstab as /etc/fstab.$$******"
